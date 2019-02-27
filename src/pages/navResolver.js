2b1b4379aa5b234f1817a9e9425350ae4155c47a
Lucas Drummond <ldrummond@wustl.edu>
2019-02-27 12:06:01 -0600

    fixing transition issues with nav

--- src/pages/navResolver.js
+++ src/pages/navResolver.js
@@ -3,20 +3,26 @@ import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
  function NavResolver(props) {
-  
   let { history, aboutVisible = false, hideAbout, showAbout} = props; 
   let classes = 'navbar'; 
-  let aboutLink = (<a className='about-link' onClick={_ => props.showAbout(history)}>About</a>)
+  
+  let onAboutClick = _ => props.showAbout(history)
 
   if(aboutVisible) {
     classes += ' light'; 
-    aboutLink = (<a className='about-link' onClick={_ => props.hideAbout(history)}>About</a>)
+    onAboutClick = _ => props.hideAbout(history)
   }
+
+  const thinArrow = 
+    (<svg className='thinArrow' viewBox="0 0 35 13">
+      <line className="line" x1="30" y1="6.8" x2="0.76" y2="6.8"/>
+      <polyline className="point" points="8.19 0.38 0.76 6.8 8.19 13.23"/>
+    </svg>)
 
   return (
     <div className={classes}>
-      <Link to='/home'>Selector</Link>
-      {aboutLink}
+      <Link className='back-link' to='/selector'>{thinArrow}Back</Link>
+      <a className='about-link' onClick={onAboutClick}>About</a>
     </div>
   )
 }
@@ -30,6 +36,7 @@ const mapDispatchToProps = dispatch => {
     hideAbout: history => {
       dispatch({type: 'HIDE_ABOUT'});
       history.push({search: ''});
+      window.scrollTo(0, 0);
     },
     showAbout: history => {
       dispatch({type: 'SHOW_ABOUT'});
