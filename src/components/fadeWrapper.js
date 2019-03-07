6d537166ed3dc8ae489b92b15f006d448b967924
Lucas Drummond <ldrummond@wustl.edu>
2019-03-07 13:08:36 -0600

    New file for fade wrapper.

---
+++ src/components/fadeWrapper.js
@@ -0,0 +1,70 @@
+import React, { Component, Children } from 'react';
+import Transition from 'react-transition-group/Transition'
+import { mergeObjects } from '../components/helperFunctions';
+
+// // https://swizec.com/blog/fade-in-lazy-loaded-images-react-css-quick-guide/swizec/8163
+// // Great FadeIn Component
+export class FadeWrapper extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = { loaded: false, };
+
+      this.defaultStyle = {
+          transition: `opacity ${this.props.duration}ms ease-in-out`,
+          opacity: 0,
+          height: 0,
+      }
+
+      this.transitionStyles = {
+          entering: { opacity: 0, height: 0, pointerEvents: "none" },
+          entered: { opacity: 1, height: "auto", pointerEvents: "all" },
+      };
+
+      this.onLoad = this.onLoad.bind(this);
+  }
+
+  onLoad() { this.setState({ loaded: true }); };
+
+  render() {
+      const { height, children, duration } = this.props,
+          { loaded } = this.state;
+
+      return (
+          <Transition in={loaded} timeout={duration}>
+              {state => children(this.onLoad, {...this.defaultStyle, ...this.transitionStyles[state]})}
+          </Transition>
+      );
+  }
+}
+
+export class SimpleFade extends Component {
+    constructor(props) {
+        super(props);
+  
+        this.defaultStyle = {
+            transition: `opacity ${this.props.duration}ms ease-in-out`,
+            opacity: 0,
+        }
+  
+        this.transitionStyles = {
+            entering: { opacity: 0, pointerEvents: "none" },
+            entered: { opacity: 1, pointerEvents: "all" },
+        };
+    }
+  
+    render() {
+        const { children, duration = 333, shouldRender } = this.props;
+        return (
+            <Transition in={shouldRender} timeout={duration}>
+                {state => 
+                    <span style={{...this.defaultStyle, ...this.transitionStyles[state]}}>
+                        {this.props.children}
+                    </span>
+                }
+            </Transition>
+        );
+    }
+  }
+
+// proptypes
--- src/components/picture.js
+++ src/components/picture.js
@@ -11,39 +11,3 @@ export default function Picture(src) {
       </picture>
   )
 }
-
-// // https://swizec.com/blog/fade-in-lazy-loaded-images-react-css-quick-guide/swizec/8163
-// // Great FadeIn Component
-export class FadeIn extends Component {
-  constructor(props) {
-      super(props);
-
-      this.state = { loaded: false, };
-
-      this.defaultStyle = {
-          transition: `opacity ${this.props.duration}ms ease-in-out`,
-          opacity: 0,
-          height: 0,
-      }
-
-      this.transitionStyles = {
-          entering: { opacity: 0, height: 0, pointerEvents: "none" },
-          entered: { opacity: 1, height: "auto", pointerEvents: "all" },
-      };
-
-      this.onLoad = this.onLoad.bind(this);
-  }
-
-  onLoad() { this.setState({ loaded: true }); };
-
-  render() {
-      const { height, children, duration } = this.props,
-          { loaded } = this.state;
-
-      return (
-          <Transition in={loaded} timeout={duration}>
-              {state => children(this.onLoad, mergeObjects(this.defaultStyle, this.transitionStyles[state]))}
-          </Transition>
-      );
-  }
-}
