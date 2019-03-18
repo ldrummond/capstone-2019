0657825fd2907d58fa9eda5bc4d7d90d43a44011
Lucas Drummond <ldrummond@wustl.edu>
2019-03-17 22:10:39 -0500

    Updates

---
+++ precache-manifest.187b2cae6306164a2c57f8003069ece1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d346955b572e0bfc94c9d9789c7377e",
+    "url": "/capstone-2019/static/media/instructionPng.5d346955.png"
+  },
+  {
+    "revision": "b8d7e9c1f7988d3d0d37",
+    "url": "/capstone-2019/static/css/main.db4fad3f.chunk.css"
+  },
+  {
+    "revision": "a35da6d86f39ef61ac0d",
+    "url": "/capstone-2019/static/js/1.a35da6d8.chunk.js"
+  },
+  {
+    "revision": "146a1c5d2934f6b24d58",
+    "url": "/capstone-2019/static/js/runtime~main.146a1c5d.js"
+  },
+  {
+    "revision": "30e228da0d158c53fad211dbf0142869",
+    "url": "/capstone-2019/static/media/car-cover.30e228da.png"
+  },
+  {
+    "revision": "e832427f2fce526ec0a5a74cc03e9c06",
+    "url": "/capstone-2019/static/media/bat-cover.e832427f.png"
+  },
+  {
+    "revision": "d0bbcd059cf88385b9199aac822968fc",
+    "url": "/capstone-2019/static/media/fish-cover.d0bbcd05.png"
+  },
+  {
+    "revision": "764e0200e6af0c19b7b732bece34ff38",
+    "url": "/capstone-2019/static/media/crowd-cover.764e0200.png"
+  },
+  {
+    "revision": "9a67b36ca12511e4aed6cdeff136abc7",
+    "url": "/capstone-2019/static/media/mold-cover.9a67b36c.png"
+  },
+  {
+    "revision": "b8d7e9c1f7988d3d0d37",
+    "url": "/capstone-2019/static/js/main.b8d7e9c1.chunk.js"
+  },
+  {
+    "revision": "8e71964e1c3fdeadebe75af3bc994229",
+    "url": "/capstone-2019/static/media/backgroundTest.8e71964e.jpg"
+  },
+  {
+    "revision": "8b3b565e0cf01cf50c77b93c03652c9b",
+    "url": "/capstone-2019/static/media/car-icon.8b3b565e.svg"
+  },
+  {
+    "revision": "a1d0b1c3213b534a135abfa3d6b043eb",
+    "url": "/capstone-2019/static/media/bat-icon.a1d0b1c3.svg"
+  },
+  {
+    "revision": "a1d0b1c3213b534a135abfa3d6b043eb",
+    "url": "/capstone-2019/static/media/fish-icon.a1d0b1c3.svg"
+  },
+  {
+    "revision": "a1d0b1c3213b534a135abfa3d6b043eb",
+    "url": "/capstone-2019/static/media/crowd-icon.a1d0b1c3.svg"
+  },
+  {
+    "revision": "a1d0b1c3213b534a135abfa3d6b043eb",
+    "url": "/capstone-2019/static/media/mold-icon.a1d0b1c3.svg"
+  },
+  {
+    "revision": "de7af7ffe35cccc6408f74cda8d31c7d",
+    "url": "/capstone-2019/static/media/arrow.de7af7ff.svg"
+  },
+  {
+    "revision": "68087588049d609ac121fe0f137ee661",
+    "url": "/capstone-2019/static/media/pentagon.68087588.svg"
+  },
+  {
+    "revision": "1ff59db504ea400f386e0d679846e9f2",
+    "url": "/capstone-2019/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9b42c845f786d65d95b56ad4224e4ec.js"
+  "/capstone-2019/precache-manifest.187b2cae6306164a2c57f8003069ece1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/capstone-2019/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
