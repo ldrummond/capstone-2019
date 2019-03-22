67cae54695d2ed547875d8501124ba9066d8fac8
Lucas Drummond <ldrummond@wustl.edu>
2019-03-22 16:01:44 -0500

    Adding button wrapper for complicated hovering

---
+++ src/components/buttonWrapper.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import classnames from 'classnames'; 
+
+export default class ButtonWrapper extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {hovering: false}
+  }
+  
+  render() {
+    return (
+      <button 
+        className={
+          classnames(
+            'button-wrapper', 
+            'unbuttoned', 
+            this.props.className, 
+            {hovering: this.state.hovering}
+          )
+        }
+        onClick={this.props.onClick}  
+      >
+        <div className='active-area' 
+          onMouseEnter={_ => this.setState({hovering: true})} 
+          onMouseLeave={_ => this.setState({hovering: false})}
+        ></div>
+        {this.props.children}
+      </button>
+    );
+  }
+}
--- src/components/svgOutline.js
+++ src/components/svgOutline.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-export function SvgOutline(props) {
+export default function SvgOutline(props) {
   const SvgComponent = props.component; 
   const {color='red', onClick=_=>{}, style={}} = props; 
   
   return (
     <span className='svg-outline-container' onClick={onClick} style={style}>
-      <div className='active-area'></div>
       <SvgComponent className='svg-outline' style={{stroke: color}} />
       <span className='fill-container'>
         <SvgComponent className='svg-fill' style={{fill: color}} />
