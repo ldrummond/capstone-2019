fce6a3ee388175cec3943bbf533968be32f4d950
Lucas Drummond <ldrummond@wustl.edu>
2019-04-03 18:11:08 -0500

    Building layout and transitions for mobile page

--- src/pages/mobilePage.js
+++ src/pages/mobilePage.js
@@ -3,6 +3,7 @@ import { ReactComponent as Squiggle } from '../assets/squiggle.svg';
 import { connect } from 'react-redux'
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { SimpleFade } from '../components/fadeWrapper';
+import ButtonWrapper from '../components/buttonWrapper';
 import classnames from 'classnames';
 import data from '../data/data';
 
@@ -18,6 +19,7 @@ class MobilePage extends Component {
     this.state = {
       mounted: false,
       hasScrolled: false,  
+      openSystemId: false, 
     }
     this.fadeDuration = 333;
   }
@@ -36,22 +38,58 @@ class MobilePage extends Component {
     }
   }
 
+  onSystemAboutClick = (e, i, systemAlreadyOpen) => {
+    if(systemAlreadyOpen) {
+      this.setState({openSystemId: false})
+    } else {
+      this.setState({openSystemId: i})
+    }
+  }
+
   render() {
     return (
       <div className={classnames('page-wrapper', 'mobile-page', {scrolled: this.state.hasScrolled})}>
         <section className='content' onScroll={this.handleScroll}>
           <div className='intro-block'>
+            <h2 className='project-title'>{process.env.REACT_APP_PROJECT_TITLE}</h2>
             <p>{data.description}</p>
           </div>
           {/* <SimpleFade className='squiggle-container' duration={this.fadeDuration} shouldRender={this.state.mounted}>
               <Squiggle />
           </SimpleFade> */}
           <div className='systems-container'>
-            {data.systems.map(system => 
-              <div key={system.path} className='system-block' style={{background: system.color}}>
-                <h3>{system.question}</h3>
-                <button>ABOUT</button>
-              </div>
+            {data.systems.map((system, i) => 
+              {
+                let isOpen = i === this.state.openSystemId; 
+                return (
+                  <div 
+                    key={system.path} 
+                    className={classnames('system-block', {open: isOpen})} 
+                    style={{background: system.color}}
+                  >
+                    <h3 className='question'>{system.question}</h3>
+                    <ButtonWrapper 
+                      className='active-button' 
+                      onClick={e => this.onSystemAboutClick(e, i, isOpen)}
+                    >
+                      <div><span className='slider'>
+                        <span>ABOUT</span><span>CLOSE</span>
+                      </span></div>
+                    </ButtonWrapper>
+                    <div className='answer-block'>
+                      <h3 className='answer'>Answer</h3>
+                      {system.interactions.map((interaction, v) => 
+                        <h4 
+                          key={`${system.path}-interaction-${v}`}
+                          style={{transitionDelay: isOpen ? `${v * 222 + 333}ms` : '0ms'}}                        
+                        >
+                          {interaction}
+                        </h4>
+                      )}
+                    </div>
+                  </div>
+                )
+              }
             )}
           </div>
           <div className='outro-block'>
