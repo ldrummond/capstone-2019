15f8cf0ffe287add2f62688ee6a01d841e03760a
Lucas Drummond <ldrummond@wustl.edu>
2019-04-22 11:19:35 -0500

    Changing shape and position of crowds

--- src/simulations/settings/crowds.js
+++ src/simulations/settings/crowds.js
@@ -13,32 +13,17 @@ export default {
   rafSettings: {fps: settingsFps},
   simulationSettings: {
     simulationType: 'crowds',
-    cursorBoidSettings: {
-      isVisible: true,
-      cursorVisible: true,
-      clearFrames: true, 
-      width: 6,
-      maxSpeed: 3,
-      // clickFn: cursorClickFn,
-      // initFn: cursorBoidInitFn,
-      // updateFn: cursorBoidUpdateFn, 
-      // drawFn: cursorBoidDrawFn,
-    },
     boidSettings: {
       isVisible: true,
       clearFrames: true, 
       drawActiveBounds: false, 
-      count: 20, 
+      count: 50, 
       maxSpeed: 1.5,
       // stroke: false,
       // strokeColor: 'white',
-      strokeWidth: 1,
+      strokeWidth: 6,
       mass: 2,
       maxForce: 50,
-      minDistance: 25,
-      maxDistance: 25,
-      avoidDistance: 80, 
-      arriveThreshold: 80,
       initFn: boidInitFn,
       updateFn: boidUpdateFn,
       drawFn: boidDrawFn, 
@@ -53,22 +38,22 @@ export default {
  */
 function boidInitFn(boidPool, bounds, otherBoidPool) {
   let points = [
-    {position: {x: bounds.width / 3 * 1, y: bounds.height / 3 * 1}, step: _ => {}, isDone: false},
-    {position: {x: bounds.width / 3 * 2, y: bounds.height / 3 * 1}, step: _ => {}, isDone: false},
-    {position: {x: bounds.width / 3 * 2, y: bounds.height / 3 * 2}, step: _ => {}, isDone: false},
-    {position: {x: bounds.width / 3 * 1, y: bounds.height / 3 * 2}, step: _ => {}, isDone: false},
+    {position: {x: bounds.width / 4 * 1, y: bounds.height / 3 * 1}, step: _ => {}, isDone: false},
+    {position: {x: bounds.width / 4 * 3, y: bounds.height / 3 * 1}, step: _ => {}, isDone: false},
+    {position: {x: bounds.width / 4 * 3, y: bounds.height / 3 * 2}, step: _ => {}, isDone: false},
+    {position: {x: bounds.width / 4 * 1, y: bounds.height / 3 * 2}, step: _ => {}, isDone: false},
   ]
 
   boidPool.map((boid, i) => {
     // boid.position.x = (Math.random() - 0.5) * 120 + (bounds.width / 4);
     // boid.position.y = (bounds.height / 2) * Math.random() + bounds.height / 4;
     if(i % 2 === 0) {
-      boid.position.x = points[0].position.x + (Math.random() - 0.5) * 30; 
+      boid.position.x = points[0].position.x + (Math.random() - 0.6) * 100; 
       boid.position.y = points[0].position.y + (Math.random() - 0.5) * 30;
       boid.followPath(points.map(point => Boid.vec2(point.position.x, point.position.y)), true); 
       boid.userData.movingClockwise = true;
     } else {
-      boid.position.x = points[3].position.x + (Math.random() - 0.5) * 30;
+      boid.position.x = points[3].position.x + (Math.random() - 0.6) * 100;
       boid.position.y = points[3].position.y + (Math.random() - 0.5) * 30;
       boid.followPath(points.map(point => Boid.vec2(point.position.x, point.position.y)).reverse(), true);
       boid.userData.movingClockwise = false;
@@ -84,60 +69,23 @@ function boidInitFn(boidPool, bounds, otherBoidPool) {
  * 
  */
 function boidUpdateFn({boid, boidPool, otherBoidPool, chaser, bounds, center}) {
-  let aBoids = boidPool.filter(boid => boid.userData.movingClockwise),
-      bBoids = boidPool.filter(boid => !boid.userData.movingClockwise);
-
-
-  // Keep boids in the crosswalks
-  let 
-    xLeft = otherBoidPool[0].position.x,
-    xRight = otherBoidPool[1].position.x,
-    yTop = otherBoidPool[0].position.y,
-    yBottom = otherBoidPool[3].position.y,
-    laneWidth = 10,
-    pos = boid.position; 
-
-  // // Inside center rect
-  // if(
-  //   pos.x < (xRight - laneWidth) && 
-  //   pos.x > (xLeft + laneWidth) &&
-  //   pos.y < (yBottom - laneWidth) &&
-  //   pos.y > (yTop + laneWidth)
-  // ) {
-  //   boid.velocity.x = 0;
-  //   boid.velocity.y = 0;
-  //   boid.flee(Boid.vec2(center.x, center.y));
-  // }
-
-  // if(
-  //   pos.x > (xRight + laneWidth) || 
-  //   pos.x < (xLeft - laneWidth) ||
-  //   pos.y > (yBottom + laneWidth) ||
-  //   pos.y < (yTop - laneWidth) 
-  // ) {
-  //   boid.velocity.x = 0;
-  //   boid.velocity.y = 0;
-  //   boid.seek(Boid.vec2(center.x, center.y));
-  // }
-   
   boidPool.map(otherBoid => {
     if(otherBoid !== boid) {
-      if(otherBoid.position.distanceSq(boid.position) < squared(14)) {
+      if(otherBoid.position.distanceSq(boid.position) < squared(17)) {
         boid.flee(otherBoid.position); 
       }
     }
   })
 
   if(boid.userData.movingClockwise) {
-    // boid.followPath([otherBoidPool[0], otherBoidPool[2]].map(point => Boid.vec2(point.position.x, point.position.y)), true);
     boid.followPath(otherBoidPool.map(point => Boid.vec2(point.position.x, point.position.y)), true);
   } else {
-    // boid.followPath([otherBoidPool[1], otherBoidPool[3]].map(point => Boid.vec2(point.position.x, point.position.y)), true);
-    // boid.followPath(otherBoidPool.map(point => Boid.vec2(point.position.x, point.position.y)).reverse(), true);
     boid.followPath(otherBoidPool.map(point => Boid.vec2(point.position.x, point.position.y)).reverse(), true);
   }
   boid.update();
 }
+
+let magnitude = 3;
 
 /**
  * 
@@ -146,23 +94,29 @@ function boidUpdateFn({boid, boidPool, otherBoidPool, chaser, bounds, center}) {
 function boidDrawFn(ctx, boid) {
 
   ctx.beginPath();
+  // if(boid.userData.movingClockwise) {
+  //   ctx.fillStyle = '#555';
+  // }
+  // else {
+  //   ctx.fillStyle = '#eee';
+  // }
   if(boid.userData.movingClockwise) {
-    ctx.fillStyle = '#555';
+    ctx.strokeStyle = '#eee';
   }
   else {
-    ctx.fillStyle = '#eee';
+    ctx.strokeStyle = '#555';
   }
   // // ctx.strokeRect(boid.position.x, boid.position.y, 5, 5);
-  // ctx.moveTo(boid.position.x - boid.velocity.x * 4, boid.position.y - boid.velocity.y * 4);
-  // ctx.lineTo(boid.position.x + boid.velocity.x * 4, boid.position.y + boid.velocity.y * 4);
-  ctx.ellipse(
-    boid.position.x, 
-    boid.position.y, 
-    4, 
-    4, 
-    boid.velocity.angle + Math.PI / 2, 
-    0, 
-    2 * Math.PI);
-  // ctx.stroke();
-  ctx.fill();
+  ctx.moveTo(boid.position.x - boid.velocity.x * magnitude, boid.position.y - boid.velocity.y * magnitude);
+  ctx.lineTo(boid.position.x + boid.velocity.x * magnitude, boid.position.y + boid.velocity.y * magnitude);
+  // ctx.ellipse(
+  //   boid.position.x, 
+  //   boid.position.y, 
+  //   4, 
+  //   4, 
+  //   boid.velocity.angle + Math.PI / 2, 
+  //   0, 
+  //   2 * Math.PI);
+  ctx.stroke();
+  // ctx.fill();
 }
